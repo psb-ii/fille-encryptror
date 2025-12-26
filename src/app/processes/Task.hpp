af8b1b0457cbd5201b24597c71156f053b13c536
Prakhar Bhadouria <prakhar.bhadouria0108@gmail.com>
2025-12-26 12:35:27 +0530

    Defined ProcessManagement class with the functionalies

--- src/app/processes/Task.hpp
+++ src/app/processes/Task.hpp
@@ -3,12 +3,15 @@
 
 #include<string>
 #include<iostream>
+#include<sstream>
 #include "../fileHandling/IO.hpp"
 
 enum class Action {
     ENCRYPT,
     DECRYPT
 };
+
+//stores custom data type of type Task
 
 struct Task{
     std:: string filePath;
@@ -18,6 +21,13 @@ struct Task{
     // && means it is volatile a it is only used for assignment here
     Task(std::fstream &&stream, Action act, std::string filePath): f_stream(std::move(stream)), action(act), filePath(filePath) {}
 
+    std:: string toString(){
+        std::ostringstream oss;
+        oss<<filePath<<","<<(action == Action::ENCRYPT ? "ENCRYPT" : "DECRYPT");
+        // test.txt ENCRYPT f_stream
+        // "test.txt,ENCRYPT"
+        return oss.str();
+    }
 
 };
 
